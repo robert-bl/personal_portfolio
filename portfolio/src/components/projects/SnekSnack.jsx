fdad28b0623c20692e803c8304996ac7d3f269fb
Robert Buskirk-Lechner <buskirklechner@gmail.com>
2023-01-11 22:39:35 -0500

    mobil test for project links

--- portfolio/src/components/projects/SnekSnack.jsx
+++ portfolio/src/components/projects/SnekSnack.jsx
@@ -1,23 +1,52 @@
 import JavaScriptIcon from "../SVGs/JavaScript"
 import HTMLIcon from "../SVGs/HTML"
 import CSSIcon from "../SVGs/CSS"
+import { AiOutlineLink, AiOutlineGithub } from "react-icons/ai"
+
+import { useState } from "react"
 
 export default function SnekSnak () {
+
+    const [showLinks, setShowLinks] = useState(false)
+
+    const toggleLinks = () => {
+        setShowLinks(!showLinks)
+    }
 
     let dimensions = '30px'
 
     return (
         <div className="w-full shadow-2xl">
             <div className='bg-drkgry text-lit p-2'>
-                <div>SnekSnack</div>
+                <div className='text-xl'>SnekSnack</div>
                 <div>Snake browser game</div>
             </div>
-            <div className="relative bg-sneksnak bg-bottom aspect-[4/3] text-drkgry">
+            <div className="relative bg-sneksnak bg-contain bg-bottom aspect-[4/3] text-drkgry" onClick={toggleLinks}>
                 <div className='absolute flex gap-1 bg-white rounded-tl-md bottom-0 right-0 p-2'>
                     <JavaScriptIcon dimensions={dimensions}/>
                     <HTMLIcon dimensions={dimensions}/>
                     <CSSIcon dimensions={dimensions}/>
                 </div>
+                {showLinks ?
+                <div className='absolute bg-white bg-opacity-80 h-full w-full'>
+                    <div className='flex justify-around items-center h-full'>
+                        <div className="text-center w-2/5">
+                            <a href='https://github.com/robert-bl/snake-browser-game' target='_blank'>
+                            <AiOutlineGithub size={40} className='mx-auto'/>
+                            <div>Github Repository</div>
+                            </a>
+                        </div>
+                        <div className="text-center w-2/5">
+                            <a href='https://snek-snack.surge.sh/' target='_blank'>
+                            <AiOutlineLink size={40} className='mx-auto'/>
+                            <div>Deployed Site</div>
+                            </a>
+                        </div>
+                    </div>
+                    <div></div>
+                </div>
+                : null
+                }
             </div>
             <div className='bg-drkgry text-lit p-2'>
                 <div>Game Logic</div>
